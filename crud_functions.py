3d974a2840ca138afd9dcb4b0f6226ad00c461a1
Vyacheslav Allabergenov <slawushkin@mail.ru>
2024-10-21 15:36:52 +0300

    Add files via upload

--- crud_functions.py
+++ crud_functions.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Создайте файл crud_functions.py и напишите там следующие функции:
 # initiate_db, которая создаёт таблицу Products, если она ещё не создана при помощи SQL запроса.
 # Эта таблица должна содержать следующие поля:
@@ -7,37 +9,45 @@
 # description(описание) - тест
 # price(цена) - целое число (не пустой)
 
-# get_all_products, которая возвращает все записи из таблицы Products, полученные при помощи SQL запроса.
+# get_all_products, которая возвращает все записи из таблицы Products, полученные при помощи
+# SQL запроса.
 
 import sqlite3
 
-con = sqlite3.connect('Products.db')
-cur = con.cursor()
+connection = sqlite3.connect('database_14.db')
+cursor = connection.cursor()
+
+cursor.executescript('''
+CREATE TABLE IF NOT EXISTS Products(
+id INT PRIMARY KEY,
+title TEXT NOT NULL,
+description TEXT,
+price INT NOT NUll
+);
+CREATE TABLE IF NOT EXISTS Users(
+id INT PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INT NOT NUll,
+balance INT NOT NUll
+);
+''')
 
 
-def initiate_db():
-
-    cur.execute('''
-    CREATE TABLE IF NOT EXISTS Products(
-    id INT PRIMARY KEY,
-    title TEXT NOT NULL,
-    descriptions TEXT,
-    price INT NOT NULL
-    );
-    ''')
+def get_all_products():
+    all_products = cursor.execute('SELECT * FROM Products;').fetchall()
+    connection.commit()
+    return all_products
 
 
-def get_all_products(title, descriptions, price):
-
-    all_product = 'SELECT * FROM Products'
-    if all_product is None:
-        for x in range(1, 5):
-            cur.execute(f'''
-            INSERT INTO VALUES ('{title[x]}', '{descriptions[x]}', '{price[x*100]}')
-            ''')
-            con.commit()
-    return all_product
+def add_products():
+    for prod in range(1, 5):
+        cursor.execute('INSERT INTO Products(title, description, price) VALUES (?, ?, ?)',
+                       (f'Product{prod}',
+                        f'Description{prod}',
+                        f'Price{prod * 100}'))
+        connection.commit()
 
 
-con.commit()
-con.close()
+connection.commit()
+# connection.close()
