b008b8404e4565923f4e258bcb8e13b050335e13
Vyacheslav Allabergenov <slawushkin@mail.ru>
2024-11-03 11:35:55 +0300

    Add files via upload

---
+++ config_2.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+API = "7774774052:AAGbFPOnluAjD2ZoIc5iFT18SYzaZWMhLRc"
+
+admin = [93372553]
+
+price_parquet_flooring_RS = 4600
+price_large_format_parquet_RS = 6000
+
+price_parquet_flooring_S = 4060
+price_large_format_parquet_S = 5550
+price_solid_wood_parquet_S = 5690
+
+price_parquet_flooring_N = 3450
+price_large_format_parquet_N = 4500
+price_solid_wood_parquet_N = 4750
+
+price_parquet_flooring_R = 2810
+price_large_format_parquet_R = 3370
+price_solid_wood_parquet_R = 4000
+
+price_fanera_works = 900
+price_parquet_works = 1500
+price_otdelka_works = 1500
